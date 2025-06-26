b94c0dd81cb49a72d6c4e26d6502cd67aa37fc1c
zhoucheng <zhoucheng133@outlook.com>
2025-06-26 20:02:04 +0800

    添加exif信息面板

---
+++ lib/components/config_item.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ConfigItem extends StatefulWidget {
+  final String keyWord;
+  final String value;
+  final bool enable;
+
+  const ConfigItem({super.key, required this.keyWord, required this.value, required this.enable});
+
+  @override
+  State<ConfigItem> createState() => _ConfigItemState();
+}
+
+class _ConfigItemState extends State<ConfigItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            widget.keyWord,
+            style: GoogleFonts.notoSansSc(
+              fontWeight: FontWeight.bold
+            ),
+          ),
+          Text(
+            widget.value,
+            style: GoogleFonts.notoSansSc(
+              color: Colors.grey,
+            ),
+            overflow: TextOverflow.ellipsis,
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/views/image_config.dart
+++ lib/views/image_config.dart
@@ -1,3 +1,4 @@
+import 'package:exif_helper/components/config_item.dart';
 import 'package:exif_helper/controllers/image_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -47,7 +48,7 @@ class _ImageConfigState extends State<ImageConfig> {
             ),
             SizedBox(width: 10,),
             Container(
-              width: 250,
+              width: 270,
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(10)
@@ -65,7 +66,16 @@ class _ImageConfigState extends State<ImageConfig> {
                         ),
                         maxLines: 2,
                         overflow: TextOverflow.ellipsis,
-                      )
+                      ),
+                      const SizedBox(height: 10,),
+                      ConfigItem(keyWord: "相机制造商", value: imageController.item.value!.make, enable: true),
+                      ConfigItem(keyWord: "相机型号", value: imageController.item.value!.model, enable: true),
+                      ConfigItem(keyWord: "镜头型号", value: imageController.item.value!.lenModel, enable: true),
+                      ConfigItem(keyWord: "焦距", value: imageController.item.value!.forcal, enable: true),
+                      ConfigItem(keyWord: "光圈", value: imageController.item.value!.fNum, enable: true),
+                      ConfigItem(keyWord: "曝光时间", value: imageController.item.value!.exposureTime, enable: true),
+                      ConfigItem(keyWord: "ISO", value: imageController.item.value!.iso, enable: true),
+                      ConfigItem(keyWord: "拍摄时间", value: imageController.item.value!.dateTime, enable: true)
                     ],
                   ),
                 ),
