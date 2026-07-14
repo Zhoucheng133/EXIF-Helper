7d3d454e2c1f4181f9a00a69e83f94921e5f4dd8
zhoucheng <zhoucheng133@outlook.com>
2026-07-15 03:08:32 +0800

    改进配置页面

--- lib/mobile/config_view.dart
+++ lib/mobile/config_view.dart
@@ -16,108 +16,122 @@ class _ConfigViewState extends State<ConfigView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.surface,
-        scrolledUnderElevation: 0.0,
-        title: Text('config'.tr),
-      ),
-      body: Obx(
-        ()=> Column(
-          children: [
-            Expanded(
-              child: Center(
-                child: imageController.load.value ? CircularProgressIndicator() : Image.memory(
-                  imageController.item.value!.raw,
-                  fit: BoxFit.contain,
-                  gaplessPlayback: false,
-                  cacheHeight: 800,
-                ),
-                ),
-              ),
-            Column(
-              mainAxisSize: MainAxisSize.min,
+    return OrientationBuilder(
+      builder: (context, orientation) {
+        final isLandscape = orientation == Orientation.landscape;
+        return Scaffold(
+          appBar: isLandscape ? null :  AppBar(
+            backgroundColor: Theme.of(context).colorScheme.surface,
+            scrolledUnderElevation: 0.0,
+            title: Text('config'.tr),
+          ),
+          body: Obx(
+            ()=> Column(
               children: [
-                ListTile(
-                  title: Text("showBrandLogo".tr),
-                  onTap: (){
-                    imageController.showLogo.value=!imageController.showLogo.value;
-                    imageController.reloadImage();
-                  },
-                  trailing: Switch(
-                    value: imageController.showLogo.value,
-                    onChanged: (value) {
-                      imageController.showLogo.value=!imageController.showLogo.value;
-                      imageController.reloadImage();
-                    },
-                  ),
-                ),
-                ListTile(
-                  title: Text("exposureTime".tr),
-                  onTap: (){
-                    imageController.showExposureTime.value=!imageController.showExposureTime.value;
-                    imageController.reloadImage();
-                  },
-                  trailing: Switch(
-                    value: imageController.showExposureTime.value,
-                    onChanged: (value) {
-                      imageController.showExposureTime.value=!imageController.showExposureTime.value;
-                      imageController.reloadImage();
-                    },
-                  ),
-                ),
-                ListTile(
-                  title: Text("fNumber".tr),
-                  onTap: (){
-                    imageController.showF.value=!imageController.showF.value;
-                    imageController.reloadImage();
-                  },
-                  trailing: Switch(
-                    value: imageController.showF.value,
-                    onChanged: (value) {
-                      imageController.showF.value=!imageController.showF.value;
-                      imageController.reloadImage();
-                    },
-                  ),
-                ),
-                ListTile(
-                  title: Text("ISO"),
-                  onTap: (){
-                    imageController.showISO.value=!imageController.showISO.value;
-                    imageController.reloadImage();
-                  },
-                  trailing: Switch(
-                    value: imageController.showISO.value,
-                    onChanged: (value) {
-                      imageController.showISO.value=!imageController.showISO.value;
-                      imageController.reloadImage();
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 15.0, right: 15.0, top: 10.0),
-                  child: Row(
-                    crossAxisAlignment: .center,
-                    mainAxisAlignment: .spaceBetween,
-                    children: [
-                      TextButton(
-                        onPressed: ()=>showImageInfo(context, imageController.item.value!), 
-                        child: Text('photoInfo'.tr)
+                Expanded(
+                  child: imageController.load.value ? Center(
+                    child: CircularProgressIndicator()
+                  ) : InteractiveViewer(
+                    minScale: 1,
+                    maxScale: 4,
+                    child: SizedBox.expand(
+                      child: Image.memory(
+                        imageController.item.value!.raw,
+                        fit: BoxFit.contain,
+                        gaplessPlayback: false,
+                        cacheHeight: 800,
                       ),
-                      FilledButton(
-                        onPressed: (){
-            
-                        }, 
-                        child: Text('saveImage'.tr)
+                    ),
+                  )
+                ),
+                if (!isLandscape) Padding(
+                  padding: .only(bottom: MediaQuery.of(context).padding.bottom),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      ListTile(
+                        title: Text("showBrandLogo".tr),
+                        onTap: (){
+                          imageController.showLogo.value=!imageController.showLogo.value;
+                          imageController.reloadImage();
+                        },
+                        trailing: Switch(
+                          value: imageController.showLogo.value,
+                          onChanged: (value) {
+                            imageController.showLogo.value=!imageController.showLogo.value;
+                            imageController.reloadImage();
+                          },
+                        ),
+                      ),
+                      ListTile(
+                        title: Text("exposureTime".tr),
+                        onTap: (){
+                          imageController.showExposureTime.value=!imageController.showExposureTime.value;
+                          imageController.reloadImage();
+                        },
+                        trailing: Switch(
+                          value: imageController.showExposureTime.value,
+                          onChanged: (value) {
+                            imageController.showExposureTime.value=!imageController.showExposureTime.value;
+                            imageController.reloadImage();
+                          },
+                        ),
+                      ),
+                      ListTile(
+                        title: Text("fNumber".tr),
+                        onTap: (){
+                          imageController.showF.value=!imageController.showF.value;
+                          imageController.reloadImage();
+                        },
+                        trailing: Switch(
+                          value: imageController.showF.value,
+                          onChanged: (value) {
+                            imageController.showF.value=!imageController.showF.value;
+                            imageController.reloadImage();
+                          },
+                        ),
+                      ),
+                      ListTile(
+                        title: Text("ISO"),
+                        onTap: (){
+                          imageController.showISO.value=!imageController.showISO.value;
+                          imageController.reloadImage();
+                        },
+                        trailing: Switch(
+                          value: imageController.showISO.value,
+                          onChanged: (value) {
+                            imageController.showISO.value=!imageController.showISO.value;
+                            imageController.reloadImage();
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 15.0, right: 15.0, top: 10.0),
+                        child: Row(
+                          crossAxisAlignment: .center,
+                          mainAxisAlignment: .spaceBetween,
+                          children: [
+                            TextButton(
+                              onPressed: ()=>showImageInfo(context, imageController.item.value!), 
+                              child: Text('photoInfo'.tr)
+                            ),
+                            FilledButton(
+                              onPressed: (){
+                  
+                              }, 
+                              child: Text('saveImage'.tr)
+                            )
+                          ],
+                        ),
                       )
                     ],
                   ),
                 )
               ],
-            )
-          ],
-        ),
-      ),
+            ),
+          ),
+        );
+      }
     );
   }
 }
