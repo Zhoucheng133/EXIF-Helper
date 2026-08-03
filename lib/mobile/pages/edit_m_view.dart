4f577eb05358498c80efa481f0d394d0fae2808c
zhoucheng <zhoucheng133@outlook.com>
2026-08-03 11:26:40 +0800

    改进移动端编辑页布局

--- lib/mobile/pages/edit_m_view.dart
+++ lib/mobile/pages/edit_m_view.dart
@@ -53,91 +53,83 @@ class _EditMViewState extends State<EditMView> {
         title: Text('editExif'.tr),
       ),
       body: Obx(
-        ()=>NestedScrollView(
-          headerSliverBuilder: (context, innerBoxIsScrolled){
-            return [
-              SliverAppBar(
-                automaticallyImplyLeading: false,
-                titleSpacing: 0,
-                toolbarHeight: 0,
-                floating: true,
-                snap: true,
-                pinned: false,
-                expandedHeight: 400,
-                flexibleSpace: FlexibleSpaceBar(
-                  background: SizedBox.expand(
-                    child: Image.file(
-                      File(imageController.filePath.value),
-                      fit: BoxFit.contain,
-                      gaplessPlayback: false,
-                      cacheHeight: 800,
-                    ),
-                  ),
-                ),
-              )
-            ];
-          }, 
-          body: ListView(
-            padding: .only(left: 15, right: 15, top: 15, bottom: 10),
-            children: [
-              EditItem(keyWord: "camMake".tr, initValue: imageController.exifData.value!.camMake, onChanged: (value) => imageController.exifData.value!.camMake=value,),
-              EditItem(keyWord: "camModel".tr, initValue: imageController.exifData.value!.camModel, onChanged: (value) => imageController.exifData.value!.camModel=value,),
-              EditItem(keyWord: "lenModel".tr, initValue: imageController.exifData.value!.lenModel, onChanged: (value) => imageController.exifData.value!.lenModel=value,),
-              EditItem(keyWord: "focal".tr, initValue: imageController.exifData.value!.focal, onChanged: (value) => imageController.exifData.value!.focal=value, endLabel: "mm", width: 100, numberOnly: true,),
-              EditItem(keyWord: "fNumber".tr, initValue: imageController.exifData.value!.fNum, onChanged: (value) => imageController.exifData.value!.fNum=value, preLabel: "F", width: 100, numberOnly: true,),
-              EditItem(keyWord: "exposureTime".tr, initValue: imageController.exifData.value!.exposureTime, onChanged: (value) => imageController.exifData.value!.exposureTime=value, endLabel: "s", width: 100, exposureTime: true,),
-              EditItem(keyWord: "ISO".tr, initValue: imageController.exifData.value!.iso, onChanged: (value) => imageController.exifData.value!.iso=value, numberOnly: true, width: 100,),
-              EditTime(),
-              Row(
-                crossAxisAlignment: .center,
-                mainAxisAlignment: .spaceBetween,
+        ()=>ListView(
+          children: [
+            SizedBox(
+              height: 400,
+              width: double.infinity,
+              child: Image.file(
+                File(imageController.filePath.value),
+                fit: BoxFit.contain,
+                gaplessPlayback: false,
+                cacheHeight: 800,
+              ),
+            ),
+            Padding(
+              padding: .only(left: 15, right: 15, top: 15, bottom: 10),
+              child: Column(
+                crossAxisAlignment: .start,
                 children: [
-                  TextButton(
-                    onPressed: (){
-                      showImageInfo(context, imageController.exifData.value!);
-                    }, 
-                    child: Text('photoInfo'.tr)
-                  ),
-                  FilledButton(
-                    onPressed: saveLoad ? null : () async {
-                      setState(() {
-                        saveLoad=true;
-                      });
-                      int timestamp = DateTime.now().millisecondsSinceEpoch ~/ 1000;
-                      final supportDir=await getApplicationDocumentsDirectory();
-                      await compute(editExif, [
-                        imageController.filePath.value, 
-                        p.join(supportDir.path, "temp_image", "${timestamp.toString()}.jpg"),
-                        imageController.exifData.value!.toJsonString(),
-                      ]);
-                      final String ext=p.extension(imageController.filePath.value);
-                      final filePath=p.join(supportDir.path, "temp_image", "${timestamp.toString()}$ext");
-                      try {
-                        await Gal.putImage(filePath);
-                        if(context.mounted){
-                          warnDialog(context, "saveSuccess".tr, "saveSuccessTip".tr);
-                        }
-                      } catch (_) {
-                        if(context.mounted){
-                          warnDialog(context, "saveFail".tr, "saveFailTip".tr);
-                        }
-                      }
-                      setState(() {
-                        saveLoad=false;
-                      });
-                    }, 
-                    child: saveLoad ? SizedBox(
-                      width: 10,
-                      height: 10,
-                      child: CircularProgressIndicator(
-                        strokeWidth: 2,
+                  EditItem(keyWord: "camMake".tr, initValue: imageController.exifData.value!.camMake, onChanged: (value) => imageController.exifData.value!.camMake=value,),
+                  EditItem(keyWord: "camModel".tr, initValue: imageController.exifData.value!.camModel, onChanged: (value) => imageController.exifData.value!.camModel=value,),
+                  EditItem(keyWord: "lenModel".tr, initValue: imageController.exifData.value!.lenModel, onChanged: (value) => imageController.exifData.value!.lenModel=value,),
+                  EditItem(keyWord: "focal".tr, initValue: imageController.exifData.value!.focal, onChanged: (value) => imageController.exifData.value!.focal=value, endLabel: "mm", width: 100, numberOnly: true,),
+                  EditItem(keyWord: "fNumber".tr, initValue: imageController.exifData.value!.fNum, onChanged: (value) => imageController.exifData.value!.fNum=value, preLabel: "F", width: 100, numberOnly: true,),
+                  EditItem(keyWord: "exposureTime".tr, initValue: imageController.exifData.value!.exposureTime, onChanged: (value) => imageController.exifData.value!.exposureTime=value, endLabel: "s", width: 100, exposureTime: true,),
+                  EditItem(keyWord: "ISO".tr, initValue: imageController.exifData.value!.iso, onChanged: (value) => imageController.exifData.value!.iso=value, numberOnly: true, width: 100,),
+                  EditTime(),
+                  Row(
+                    crossAxisAlignment: .center,
+                    mainAxisAlignment: .spaceBetween,
+                    children: [
+                      TextButton(
+                        onPressed: (){
+                          showImageInfo(context, imageController.exifData.value!);
+                        }, 
+                        child: Text('photoInfo'.tr)
+                      ),
+                      FilledButton(
+                        onPressed: saveLoad ? null : () async {
+                          setState(() {
+                            saveLoad=true;
+                          });
+                          int timestamp = DateTime.now().millisecondsSinceEpoch ~/ 1000;
+                          final supportDir=await getApplicationDocumentsDirectory();
+                          await compute(editExif, [
+                            imageController.filePath.value, 
+                            p.join(supportDir.path, "temp_image", "${timestamp.toString()}.jpg"),
+                            imageController.exifData.value!.toJsonString(),
+                          ]);
+                          final String ext=p.extension(imageController.filePath.value);
+                          final filePath=p.join(supportDir.path, "temp_image", "${timestamp.toString()}$ext");
+                          try {
+                            await Gal.putImage(filePath);
+                            if(context.mounted){
+                              warnDialog(context, "saveSuccess".tr, "saveSuccessTip".tr);
+                            }
+                          } catch (_) {
+                            if(context.mounted){
+                              warnDialog(context, "saveFail".tr, "saveFailTip".tr);
+                            }
+                          }
+                          setState(() {
+                            saveLoad=false;
+                          });
+                        }, 
+                        child: saveLoad ? SizedBox(
+                          width: 10,
+                          height: 10,
+                          child: CircularProgressIndicator(
+                            strokeWidth: 2,
+                          )
+                        ) : Text('saveImage'.tr)
                       )
-                    ) : Text('saveImage'.tr)
-                  )
+                    ],
+                  ),
                 ],
-              )
-            ],
-          )
+              ),
+            )
+          ],
         )
       ),
     );
