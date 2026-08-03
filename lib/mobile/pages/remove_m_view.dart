7dea677ea13a93dbf7cd0171188bd98d1748e5bb
zhoucheng <zhoucheng133@outlook.com>
2026-08-03 10:00:17 +0800

    移除exif页面禁用横屏

--- lib/mobile/pages/remove_m_view.dart
+++ lib/mobile/pages/remove_m_view.dart
@@ -5,6 +5,7 @@ import 'package:exif_helper/controllers/libs.dart';
 import 'package:exif_helper/functions/dialog_func.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
@@ -19,101 +20,116 @@ class RemoveMView extends StatefulWidget {
 }
 
 class _RemoveMViewState extends State<RemoveMView> {
+
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight
+    ]);
+    super.dispose();
+  }
+
   final imageController=Get.find<ImageController>();
 
   bool saveLoad=false;
 
   @override
   Widget build(BuildContext context) {
-    return OrientationBuilder(
-      builder: (context, orientation) {
-        final isLandscape = orientation == Orientation.landscape;
-        return Scaffold(
-          appBar: isLandscape ? null :  AppBar(
-            backgroundColor: Theme.of(context).colorScheme.surface,
-            scrolledUnderElevation: 0.0,
-            title: Text('removeExif'.tr),
-          ),
-          body: Obx(
-            ()=> Column(
-              children: [
-                Expanded(
-                  child: InteractiveViewer(
-                    minScale: 1,
-                    maxScale: 4,
-                    child: SizedBox.expand(
-                      child: Image.file(
-                        File(imageController.filePath.value),
-                        fit: BoxFit.contain,
-                        gaplessPlayback: false,
-                        cacheHeight: 800,
-                      ),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        scrolledUnderElevation: 0.0,
+        title: Text('removeExif'.tr),
+      ),
+      body: Obx(
+        ()=> Column(
+          children: [
+            Expanded(
+              child: InteractiveViewer(
+                minScale: 1,
+                maxScale: 4,
+                child: SizedBox.expand(
+                  child: Image.file(
+                    File(imageController.filePath.value),
+                    fit: BoxFit.contain,
+                    gaplessPlayback: false,
+                    cacheHeight: 800,
+                  ),
+                ),
+              )
+            ),
+            Padding(
+              padding: .only(bottom: MediaQuery.of(context).padding.bottom),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 15.0, right: 15.0, top: 10.0),
+                    child: Row(
+                      crossAxisAlignment: .center,
+                      mainAxisAlignment: .spaceBetween,
+                      children: [
+                        TextButton(
+                          onPressed: (){
+                            showImageInfo(context, imageController.exifData.value!);
+                          }, 
+                          child: Text('photoInfo'.tr)
+                        ),
+                        FilledButton(
+                          onPressed: saveLoad ? null : () async {
+                            setState(() {
+                              saveLoad=true;
+                            });
+                            int timestamp = DateTime.now().millisecondsSinceEpoch ~/ 1000;
+                            final supportDir=await getApplicationDocumentsDirectory();
+                            await compute(
+                              removeExif, 
+                              [imageController.filePath.value, p.join(supportDir.path, "temp_image", "${timestamp.toString()}.jpg")]
+                            );
+                            final String ext=p.extension(imageController.filePath.value);
+                            final filePath=p.join(supportDir.path, "temp_image", "${timestamp.toString()}$ext");
+                            try {
+                              await Gal.putImage(filePath);
+                              if(context.mounted){
+                                warnDialog(context, "saveSuccess".tr, "saveSuccessTip".tr);
+                              }
+                            } catch (_) {
+                              if(context.mounted){
+                                warnDialog(context, "saveFail".tr, "saveFailTip".tr);
+                              }
+                            }
+                            setState(() {
+                              saveLoad=false;
+                            });
+                          }, 
+                          child: saveLoad ? SizedBox(
+                            width: 10,
+                            height: 10,
+                            child: CircularProgressIndicator(
+                              strokeWidth: 2,
+                            )
+                          ) : Text('saveImage'.tr)
+                        )
+                      ],
                     ),
                   )
-                ),
-                if (!isLandscape) Padding(
-                  padding: .only(bottom: MediaQuery.of(context).padding.bottom),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 15.0, right: 15.0, top: 10.0),
-                        child: Row(
-                          crossAxisAlignment: .center,
-                          mainAxisAlignment: .spaceBetween,
-                          children: [
-                            TextButton(
-                              onPressed: (){
-                                showImageInfo(context, imageController.exifData.value!);
-                              }, 
-                              child: Text('photoInfo'.tr)
-                            ),
-                            FilledButton(
-                              onPressed: saveLoad ? null : () async {
-                                setState(() {
-                                  saveLoad=true;
-                                });
-                                int timestamp = DateTime.now().millisecondsSinceEpoch ~/ 1000;
-                                final supportDir=await getApplicationDocumentsDirectory();
-                                await compute(
-                                  removeExif, 
-                                  [imageController.filePath.value, p.join(supportDir.path, "temp_image", "${timestamp.toString()}.jpg")]
-                                );
-                                final String ext=p.extension(imageController.filePath.value);
-                                final filePath=p.join(supportDir.path, "temp_image", "${timestamp.toString()}$ext");
-                                try {
-                                  await Gal.putImage(filePath);
-                                  if(context.mounted){
-                                    warnDialog(context, "saveSuccess".tr, "saveSuccessTip".tr);
-                                  }
-                                } catch (_) {
-                                  if(context.mounted){
-                                    warnDialog(context, "saveFail".tr, "saveFailTip".tr);
-                                  }
-                                }
-                                setState(() {
-                                  saveLoad=false;
-                                });
-                              }, 
-                              child: saveLoad ? SizedBox(
-                                width: 10,
-                                height: 10,
-                                child: CircularProgressIndicator(
-                                  strokeWidth: 2,
-                                )
-                              ) : Text('saveImage'.tr)
-                            )
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
-        );
-      }
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
